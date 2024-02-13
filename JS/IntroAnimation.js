f6c058702abaf9802382f0c2b0bf49481953f018
Kisu <kisuzoid@gmail.com>
2024-02-14 03:32:05 +0530

    modify

---
+++ JS/IntroAnimation.js
@@ -0,0 +1,47 @@
+const professions = [
+    "A Programmer",
+    "A Graphic Designer",
+    "An Animator",
+    "A Digital Artist",
+    "A Writter"
+];
+
+let currentIndex = 0;
+let currentText = "";
+let letterIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const profession = professions[currentIndex];
+
+    if (isDeleting) {
+        currentText = profession.substring(0, letterIndex - 1);
+        letterIndex--;
+    } else {
+        currentText = profession.substring(0, letterIndex + 1);
+        letterIndex++;
+    }
+
+    document.getElementById("profession").textContent = `"${currentText}"`;
+
+    let typingSpeed = 100; // Adjust typing speed here
+
+    if (!isDeleting && letterIndex === profession.length + 1) {
+        isDeleting = true;
+        typingSpeed = 2000; // Set pause before deleting
+    } else if (isDeleting && letterIndex === 0) {
+        isDeleting = false;
+        currentIndex = (currentIndex + 1) % professions.length;
+        typingSpeed = 100; // Set pause before next word
+    }
+
+    if (isDeleting && letterIndex === 0) {
+        document.getElementById("profession").classList.remove('disappear');
+    }
+
+    setTimeout(type, typingSpeed);
+}
+
+window.onload = function() {
+    type();
+};
